70e69fb4a59d27eab6d583c085ec9ea46c359543
Rainacam12 <Rainacam12@github.com>
2021-10-27 21:52:10 -0700

    added veggies element kale

--- trackedfolder/tracked.js
+++ trackedfolder/tracked.js
@@ -3,11 +3,17 @@ console.log(helloJs);
 alert('hello js');
 const fruits = ["bananas", "apples", "strawberries"];
 console.log(fruits);
+
+//added array veggies
+const veggies = ["cucumbers", "broccolis", "kale", "other nasty shit"];
+console.log(veggies);
+
 //found the bug: needed explicit return to hoist object
 const fruitSalad = fruits.map(fruit => {
     return { salad: `sliced ${fruit}` }
 });
 console.log(fruitSalad);
+
 const fruitPies = fruits.map(fruit => {
     return { pie: `${fruit} pie` }
 });
