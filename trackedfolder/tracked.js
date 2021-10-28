24add69eb37be9e322f2ac962b755b92a04fa22e
luigicampbell <luigicampbell@github.com>
2021-10-27 19:57:03 -0700

    made a fruit salad

--- trackedfolder/tracked.js
+++ trackedfolder/tracked.js
@@ -3,3 +3,5 @@ console.log(helloJs);
 alert('hello js');
 const fruits = ["bananas", "apples", "strawberries"];
 console.log(fruits);
+
+const fruitSalad = fruits.map(fruit => {salad: `sliced ${fruit}`});
