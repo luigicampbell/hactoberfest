c269dd77856834a6c6cfe5bcb0ff04cb656c6517
Rainacam12 <Rainacam12@github.com>
2021-10-27 23:14:56 -0700

    added for loop for veggie var

--- trackedfolder/tracked.js
+++ trackedfolder/tracked.js
@@ -7,7 +7,10 @@ console.log(fruits);
 //added array veggies
 const veggies = ["cucumbers", "broccolis", "kale", "other nasty shit"];
 console.log(veggies);
-
+//for loop "a lot of [] around here"
+for (var i = 0; i < veggies.length; i++) { 
+    console.log("A lot of " + veggies[i] + " around here.");
+}
 //found the bug: needed explicit return to hoist object
 const fruitSalad = fruits.map(fruit => {
     return { salad: `sliced ${fruit}` }
@@ -18,7 +21,3 @@ const fruitPies = fruits.map(fruit => {
     return { pie: `${fruit} pie` }
 });
 console.log(fruitPies);
-
-//spliced veggies
-const veggiesSpliced = veggies.splice(0,2);
-console.log(veggiesSpliced);
